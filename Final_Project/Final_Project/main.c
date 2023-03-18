65e8880c8225cf00c0122430a79e84ab5e2cc861
AhmedAtia1507 <104103615+AhmedAtia1507@users.noreply.github.com>
2023-03-18 20:41:08 +0200

    Update main.c

--- Final_Project/Final_Project/main.c
+++ Final_Project/Final_Project/main.c
@@ -27,42 +27,69 @@
 
 #define F_CPU 16000000UL
 #include <util/delay.h>
+
 void LM35_StartConversion(void);
 void LM35_TempControl(void);
+
 int main(void)
 {
 	u8 Loc_u8Value = 0;
+	/*Here, We initialize the peripherals used*/
 	LCD_Init();
 	SRVM_Init();
 	BUZZER_Init();
 	GI_Enable();
-
+	/*We set the ADC channel 0 pin to be input*/
 	DIO_SetPinDirection(DIO_PORTA, DIO_PIN0, DIO_PIN_INPUT);
+	/*We set the pin connected to relay to be output*/
 	DIO_SetPinDirection(DIO_PORTA, DIO_PIN2, DIO_PIN_OUTPUT);
+	/*We initialize timer 0 in Normal mode and set the ADC start conversion function as the Overflow*/
+	/*Interrupt function*/
 	TMR_Timer0Init();
-	TMR_Timer0_OVF_SetCallBack(LM35_StartConversion);
+	TMR_Timer0_OVF_SetCallBack(LM35_StartConversion); //Timer trigger ADC
 	TMR_Timer0Start();
+	/*We initialize ADC peripheral as single conversion mode and set the temperature control function as*/
+	/*The ADC conversion complete interrupt function*/
 	ADC_ConversionCompleteSetCallback(LM35_TempControl);
 	ADC_Init();
 	KPD_Init();
+	/*We initialize timer 2 to which its OC pin connected to dimming LED*/
 	TMR_Timer2Init();
+
+	/*We set the Rx Pin of UART Peripheral to be input "Receive whats comming from bluetooth module"*/
+	/*We set the Tx pin of UART Peripheral to be output "Send Strings which we want to display on mobile"*/
 	DIO_SetPinDirection(DIO_PORTD, DIO_PIN0, DIO_PIN_INPUT); //Rx
 	DIO_SetPinDirection(DIO_PORTD, DIO_PIN1, DIO_PIN_OUTPUT); //Tx
+
+	/*We set the Interface function as the UART Rx Complete Interrupt Function*/
 	UART_RxCompleteSetCallback(HC05_AdminUserInterface);
+	/*Then, We initialize UART*/
 	UART_Init();
+
+	/*We Check to see if the Admin Username and password are already saved in the memory or not*/
 	Internal_EEPROM_ReadByte(HC05_ADMIN_SAVED_STATUS_MEM_ADDRESS, &Loc_u8Value);
+
+	/*
+		As a default, we set the Admin's information as follows:
+		Username: 2
+		Password: 123
+	*/
 	if (1 != Loc_u8Value)
 	{
+		/*Means that the Admin's information is not saved and needs to be saved*/
 		LCD_WriteString("First Entry!");
 		_delay_ms(2000);
 		LCD_ClearDisplay();
 		HC05_AdminUsernamePasswordSaver();
 	}
+	/*We send the welcoming message on both the LCD and Mobile*/
 	USER_PROGRAM_START();
 	HC05_WelcomeMessage();
+
 	u8 Loc_u8UserInput = KPD_NOT_PRESSED;
     while (1) 
     {
+		/*Here, we check to see if the user wants to login or not*/
 		LCD_WriteString("WANT TO LOGIN?");
 		
 		LCD_SetCursorPosition(LCD_SECOND_ROW, 0);
@@ -75,10 +102,13 @@ int main(void)
 		}
 		if('1' == Loc_u8UserInput)
 		{
+			/*Means that the user wants to login*/
 			USER_APP();
 		}
 		else if('2' == Loc_u8UserInput)
 		{
+			/*Means that the user doesn't want to login at the moment*/
+			/*In that case, we display the status of the devices on LCD*/
 			LCD_ClearDisplay();
 			HC05_ShowRunningDevices();
 		}
@@ -93,16 +123,28 @@ int main(void)
 
 void LM35_StartConversion(void)
 {
+	/*We set timer 0 to start ADC conversion approximately every 4ms which is more than the ADC peripheral*/
+	/*needs to convert analog signal coming from temperature sensor (max. 260 us)*/
 	ADC_StartConversion(ADC_CHANNEL_0);
-
 }
 /*LM35 Temp Sensor gives us 10mv/degree*/
+/*-15 ---> 55*/
 void LM35_TempControl(void)
 {
 	u16 Loc_u16ADCValue = 0;
 	ADC_GetDigitalValueAsynchronous(&Loc_u16ADCValue);
-	u8 Loc_u8Temp = Loc_u16ADCValue / 4; //LM35
-	
+	/*
+							ADC_DigitalValue * 2.56
+		ADC_Voltage_Value = -----------------------
+									1024
+		LM35_Temperature = ADC_Voltage_Value * 100
+		So, LM35_Temperature approximately equal to ADC_DigitalValue / 4
+	*/
+	u8 Loc_u8Temp = Loc_u16ADCValue / 4; 
+	/*
+		If Temperature is greater than 28 degrees ----> Turn on Air conditioner "Relay"
+		If Temperature is less than 21 degrees ----> Turn off Air conditioner "Relay"
+	*/
 	if(Loc_u8Temp < 21)
 	{
 		DIO_SetPinValue(DIO_PORTA,DIO_PIN2,DIO_PIN_LOW);
